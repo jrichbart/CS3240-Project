63ca3d8b7dc9098a97446f1ba1dccabc9900feb3
Ethan Choo <ec4kj@virginia.edu>
2020-09-15 20:26:32 -0400

    added settings for Heroku deployment

--- virtualstudybuddy/settings.py
+++ virtualstudybuddy/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
+import django_heroku
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
