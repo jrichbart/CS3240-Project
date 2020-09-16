0b7c7e6d995b13129722fb82c1f740747bac9cba
Ethan Choo <ec4kj@virginia.edu>
2020-09-15 20:20:20 -0400

    added model for Accounts and simple non functional login and accounts page

---
+++ accounts/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+def login(request):
+    template = loader.get_template('accounts/login.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+
+def account(request):
+    template = loader.get_template('accounts/accounts.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
