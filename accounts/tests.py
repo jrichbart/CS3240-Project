fa47c2725a28f9471af16ff18012e20bd8129158
Ethan Choo <ec4kj@virginia.edu>
2020-09-29 12:57:12 -0400

    added redundant test for looking into Travis-CI

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+class AccountModelTests(TestCase):
+
+    def test_true(self):
+        #Test only for purpose of looking into Travis-CI
+        #Delete later
+        self.assertIs(True, True)
