1daac5f3023f69eb0bff4ea6afa9b39e376adf4a
jrichbart <jar5rxc@virginia.edu>
2020-10-15 21:55:55 -0400

    Can now see usernames on page

--- find/views.py
+++ find/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
-
-
+from django.contrib.auth.models import User
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_account_list = User.objects.order_by('-username')[:5]
+    context = {'latest_account_list': latest_account_list}
+    return render(request, 'find/index.html', context)
