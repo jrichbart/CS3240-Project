97c1ce73b7c96c82d86849873bb77ac2a0e6d9d7
Ethan Choo <ec4kj@virginia.edu>
2020-11-04 17:20:31 -0500

    added buddies to admin console

--- userAccount/admin.py
+++ userAccount/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import userAccount, Course, Availability
+from .models import userAccount, Course, Availability, buddies
 
 admin.site.register(userAccount)
 
 admin.site.register(Course)
 
 admin.site.register(Availability)
+
+admin.site.register(buddies)
