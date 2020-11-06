98d1e72993797aefdc08d586471aef6417e1b328
jrichbart <jar5rxc@virginia.edu>
2020-11-06 12:19:37 -0500

    url update

--- find/urls.py
+++ find/urls.py
@@ -6,5 +6,6 @@ app_name = 'find'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    
+    path('<username>/', views.view_send_request, name='views_send_request'),
+    path('<username>/', views.send_buddy_request, name='views_send_request'),
 ]
